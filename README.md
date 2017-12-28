e1c70c31feedddabe9cee82a30625fb48a2975c8
antonloof <anton@loofarna.se>
2017-12-28 15:28:47 +0100

    sync

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # lnpsController
+
+Rest api + web service for controlling EA powersupplies and leds.
+
+INSTALL:
+1. Install the libs specified in libs.txt
+2. Create config.json that specifies which com ports, and powers supplies are available, config_example.json contains an example of this file
+3. main.py starts the rest server 
+4. The web files are located in /web, host them however you like 
--- web/js/config.js
+++ web/js/config.js
@@ -1,7 +1,33 @@
 $(function() {
 	$.getJSON("http://localhost:5049/api/v1/dev?callback=?", function (devs) {
-		for (int i = 0; i < devs.length; i++) {
-			$("#devList").append('<li>Device no: ' + devs[i] + '</li>');
+		for (var i = 0; i < devs.length; i++) {
+			var item = $('<li><span class="text"></span><table></table></li>');
+			$.getJSON("http://localhost:5049/api/v1/dev/" + devs[i] + "/name?callback=?", function (name) {
+				item.children(".text").text(name);
+			});
+			for (var j = 0; j < inputs.length; j++) {
+				var row = $("<tr></tr>");
+				row.append("<td>" + inputs[i].displayName + "</td>");
+				row.append('<td><input name="' + name + '" /></td>');
+				button = $("<button></button>");
+				button.click(function () {
+					$(this).parent().parent().find('input[name="' + inputs[i].name + '"]')
+				});
+			}
+			item.children("table").ap
+			item.data("dev", devs[i]);
+			item.click(function () {
+				$(this).children(".expanding").show()
+			});
+			$("#devList").append(item);
 		}
 	});
 });
+
+var inputs = [
+	{
+		displayName: "Name",
+		name: "name",
+		url: "name",
+	}
+];
